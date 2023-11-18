0764bcaf4f86636a5d484a0b5256b1b29c30aa8b
Isabelle Oliveira <isabelle.caroll.io@gmail.com>
2023-11-18 19:19:23 -0300

    Add PlanoContaController, PlanoConta, Transacao, MyFinanceDBContext, IPlanoContaService, and Index view for PlanoConta

---
+++ src/myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using myfinance_web_netcore.Models;
+
+namespace myfinance_web_netcore.Controllers;
+
+public class PlanoContaController : Controller
+{
+    private readonly ILogger<PlanoContaController> _logger;
+
+    public PlanoContaController(ILogger<PlanoContaController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ src/myfinance-web-netcore/Infrastructure/MyFinanceDBContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using myfinance_web_netcore.Domain;
+
+namespace myfinance_web_netcore.Infrastructure
+{
+    public class MyFinanceDBContext : DbContext
+    {
+        public DbSet<PlanoConta> PlanoConta { get; set; }
+        public DbSet<Transacao> Transacao { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Server=localhost\sqlexpress;Database=myfinanceweb;Trusted_Connection=True;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
