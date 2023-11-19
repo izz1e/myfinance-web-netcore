211de93cc4508439b9fbc5828306eceabf068381
Isabelle Oliveira <isabelle.caroll.io@gmail.com>
2023-11-18 22:15:15 -0300

    Add AutoMapper configuration and update PlanoContaController

--- src/myfinance-web-netcore/Controllers/PlanoContaController.cs
+++ src/myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -1,26 +1,67 @@
 ﻿using System.Diagnostics;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using myfinance_web_netcore.Models;
 using myfinance_web_netcore.Services;
 
 namespace myfinance_web_netcore.Controllers;
 
+[Route("[controller]")]
 public class PlanoContaController : Controller
 {
     private readonly ILogger<PlanoContaController> _logger;
+    private readonly IMapper _mapper;
     private readonly IPlanoContaService _planoContaService;
 
-    public PlanoContaController(ILogger<PlanoContaController> logger, IPlanoContaService planoContaService)
+    public PlanoContaController(ILogger<PlanoContaController> logger, IMapper mapper,IPlanoContaService planoContaService)
     {
         _logger = logger;
+        _mapper = mapper;
         _planoContaService = planoContaService;
     }
 
+    [HttpGet]
+    [Route("Index")]
     public IActionResult Index()
     {
-        ViewBag.ListaPlanoContas = _planoContaService.ListarPlanoContas();
+        var listaPlanoConta = _planoContaService.ListarPlanoContas();
+        var lista = _mapper.Map<IEnumerable<PlanoContaModel>>(listaPlanoConta);
+        ViewBag.ListaPlanoContas = lista;
+
         return View();
     }
+
+    [HttpGet]
+    [Route("Cadastro")]
+    [Route("Cadastro/{id}")]
+    public IActionResult Cadastro(int? id)
+    {
+        if (id != null)
+        {
+            //var registro = _planoContaService.RetornarRegistro((int)id);
+            //return View(registro);
+        }
+        return View();
+    }
+
+    [HttpPost]
+    [Route("Cadastro")]
+    [Route("Cadastro/{id}")]
+    public IActionResult Cadastro(PlanoContaModel model)
+    {
+        //_planoContaService.Salvar(model);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    [Route("Excluir/{id}")]
+    public IActionResult Excluir(int id)
+    {
+        //_planoContaService.Excluir(id);
+        return RedirectToAction("Index");
+    }
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
